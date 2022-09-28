615211f1d05c25dccb77d743d0c387d5486a42d1
Tan <Taan3311@gmail.com>
2022-09-27 18:34:44 -0700

    get all current user spots done

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { setTokenCookie, requireAuth,restoreUser } = require('../../utils/auth');
 const router = express.Router();
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
@@ -7,6 +7,7 @@ const { Spot } = require('../../db/models');
 const { SpotImage } = require('../../db/models');
 const { Review } = require('../../db/models');
 const {sequelize} = require('../../db/models')
+const {User} = require('../../db/models')
 
 
 
@@ -40,7 +41,33 @@ router.get('/', async (req, res) => {
 }
   );
 
-
+  router.get('/current',requireAuth, async (req, res) => {
+    const newSpot = {}
+    newSpot.Spot = await Spot.findAll({
+        raw:true,
+        where:{ownerId:req.user.id}
+    })
+   
+    for (const spott of newSpot.Spot) {
+        const avg = await Review.findAll({
+            where: { spotId: spott.id},
+            attributes: [[sequelize.fn('AVG', sequelize.col('stars')), 'average']],
+            raw:true
+        })
+        const allowPreview = await SpotImage.findOne({
+            where: {spotId: spott.id,preview:true },
+            attributes:['url'],
+            raw:true
+        })
+        console.log(avg[0].average)
+        console.log(typeof(avg[0].average))
+       spott.avgRating = (Number(avg[0].average).toFixed(1))
+       if(allowPreview){
+        spott.previewImage = allowPreview.url
+       }
+    }
+    return res.json(newSpot)
+  })
 
 
 
