31a186e2c0cab3edb100b20039d92d05edcf2b03
Tan <Taan3311@gmail.com>
2022-09-28 10:50:40 -0700

    get all reviews from current herokutestingp3

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -9,44 +9,43 @@ const { Review,ReviewImage } = require('../../db/models');
 const {sequelize} = require('../../db/models')
 const {User} = require('../../db/models')
 
-// router.get('/current',requireAuth, async (req, res) => {
-    
-//     const newSpot = {}
-//     newSpot.Reviews = await Review.findAll({
-//         raw:true,
-//         where:{userId:req.user.id}
-//     })
-//     for (const spott of newSpot.Reviews) {
-//         const spotReviewd = await Spot.findOne({
-//             where: {id: spott.id,},
-//             attributes:['id','ownerId','address','city','state','country','lat','lng','name','price'],
-//             raw:true
-//         })
-//         const user = await User.findOne({
-//             where: {id: spott.id},
-//             attributes:['id',"firstName","lastName"],
-//             raw:true
-//         })
-//         const allowPreview = await SpotImage.findOne({
-//             where: {spotId: spott.id,preview:true },
-//             attributes:['url'],
-//             raw:true
-//         })
-//         const image = await ReviewImage.findAll({
-//             where: {reviewId: spott.id,},
-//             attributes:['id','url'],
-//             raw:true
-//         })
-//        spott.User = user
-//        spott.Spot = spotReviewd
-//        spotReviewd.previewImage = allowPreview
-//        if(allowPreview){
-//         spotReviewd.previewImage = allowPreview
-//        }
-//        spott.ReviewImages = image
-//     }
-//     return res.json(newSpot)
-//   })
+router.get('/current',requireAuth, async (req, res) => {
+    const newSpot = {}
+    newSpot.Reviews = await Review.findAll({
+        raw:true,
+        where:{userId:req.user.id}
+    })
+    for (const spott of newSpot.Reviews) {
+        const spotReviewd = await Spot.findOne({
+            where: {id: spott.id,},
+            attributes:['id','ownerId','address','city','state','country','lat','lng','name','price'],
+            raw:true
+        })
+        const user = await User.findOne({
+            where: {id: spott.id},
+            attributes:['id',"firstName","lastName"],
+            raw:true
+        })
+        const allowPreview = await SpotImage.findOne({
+            where: {spotId: spott.id,preview:true },
+            attributes:['url'],
+            raw:true
+        })
+        const image = await ReviewImage.findAll({
+            where: {reviewId: spott.id,},
+            attributes:['id','url'],
+            raw:true
+        })
+       spott.User = user
+       spott.Spot = spotReviewd
+       spotReviewd.previewImage = allowPreview
+       if(allowPreview){
+        spotReviewd.previewImage = allowPreview
+       }
+       spott.ReviewImages = image
+    }
+    return res.json(newSpot)
+  })
 
 
 
