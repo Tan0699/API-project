161ce20c8b3fd3dd56f695ae4610ad586304cdf2
Tan <Taan3311@gmail.com>
2022-09-29 09:33:01 -0700

    heroku crash fix test 1

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -12,7 +12,7 @@ const bookingsRouter = require('./bookings.js');
   // If current user session is valid, set req.user to the user in the database
   // If current user session is not valid, set req.user to null
 router.use(restoreUser);
-
+//...
 router.use('/session', sessionRouter);
 
 router.use('/spots', spotsRouter);
