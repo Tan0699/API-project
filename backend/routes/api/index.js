f3899e886a2fd1d4dd8d86f0be0d148ec1973848
Tan <Taan3311@gmail.com>
2022-09-29 10:03:37 -0700

    heroku crash test fix 7

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -7,7 +7,7 @@ const usersRouter = require('./users.js');
 const { restoreUser } = require("../../utils/auth.js");
 const spotsRouter = require('./spots.js');
 const reviewsRouter = require('./reviews.js');
-const bookingsRouter = require('./bookings.js');
+// const bookingsRouter = require('./bookings.js');
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
   // If current user session is not valid, set req.user to null
@@ -18,7 +18,7 @@ router.use('/session', sessionRouter);
 router.use('/spots', spotsRouter);
 router.use('/reviews', reviewsRouter);
 router.use('/users', usersRouter);
-router.use('/bookings', bookingsRouter);
+// router.use('/bookings', bookingsRouter);
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
