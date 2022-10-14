d595b90b7297c27eef745cb0d05c082e976d4822
Tan <Taan3311@gmail.com>
2022-10-14 11:22:21 -0700

    fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
-
+//....
   return isLoaded && (
     <Switch>
       <Route path="/login">
