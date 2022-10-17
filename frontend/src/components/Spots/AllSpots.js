372b0c4bce3043546e96e5d89d4a2f0fde2cd1ac
Tan <Taan3311@gmail.com>
2022-10-17 16:25:04 -0700

    half of getallspots

--- frontend/src/components/Spots/AllSpots.js
+++ frontend/src/components/Spots/AllSpots.js
@@ -1,4 +1,32 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import { useDispatch,useSelector } from 'react-redux';
+import { getAllSpots } from '../../store/SpotsReducer';
+import { useEffect } from 'react';
+const dispatch = useDispatch()
+const ShowAllSpots = ()=> {
+const theSpots = useSelector(state => {
+    state.spots
+})
+const dispatch = useDispatch()
+useEffect(()=>{
+    dispatch(getAllSpots())
+},[dispatch])
+}
+
+return (
+    <div className="Container">
+            <div className="alltheSpots">
+                {theSpots.map(spot =>{
+                <div key={spot.id} className="oneSpot">
+                    <NavLink to={`/spots/${spot.id}`}>{spot.name}</NavLink>
+                </div>
+                })}
+            </div>
+    </div>
+)
+
+
+
 
 
 export default AllSpots;
