3b3272504d3ae215573605184eb1bc7496b3a480
Tan <Taan3311@gmail.com>
2022-10-17 14:39:47 -0700

    added spotsReducer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
-
+import GetAllSpots from "./components/Spots/AllSpots"
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
@@ -17,6 +17,9 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
+          <Route path='/'>
+            <GetAllSpots/>
+          </Route>
           {/* <Route path="/signup"> */}
             {/* <SignupFormPage /> */}
           {/* </Route> */}
---
+++ frontend/src/store/SpotsReducer.js
@@ -0,0 +1,41 @@
+import { csrfFetch } from './csrf';
+
+const ALL_SPOTS = 'spots/allSpots'
+const OWNER_SPOTS = 'spots/ownerSpots'
+const EDIT_SPOT = 'spots/editSpot'
+const DELETE_SPOT = 'spots/deleteSpot'
+const CREATE_SPOT = 'spots/createSpot'
+const SPOT_DETAILS = 'spots/spotDetails'
+
+const allSpots = (spots) =>{
+    return {
+        type:ALL_SPOTS,
+        spots
+    }
+}
+export const getAllSpots = () => async dispatch => {
+    const response = await fetch(`/api/spots`);
+  
+    if (response.ok) {
+      const list = await response.json();
+      dispatch(allSpots(list));
+    }
+  };
+  const initialState = {
+    everySpot: [],
+    oneSpot: []
+  };
+
+  export default SpotsReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case ALL_SPOTS: 
+        const newState = {...state};
+        action.everySpot.forEach(spot => {
+          newState[spot.id] = spot;
+        });
+        return newState
+          ;
+      default:
+        return state;
+    }
+  }
